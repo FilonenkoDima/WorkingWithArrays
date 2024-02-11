b77d76e9e14b0f6cb1efe031076d72b548d3ed26
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-02-11 20:59:26 +0200

    Looping Arrays forEach

---
+++ LoopingArrays_forEach/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log("=========== FOREACH ===========");
+
+movements.forEach(function (movement, index, array) {
+  if (movement > 0) {
+    console.log(`Movement ${index + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${index + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+});
