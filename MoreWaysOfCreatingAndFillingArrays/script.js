5e5336be995f394ca2dc27049200af0855030ff1
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-02-16 23:44:23 +0200

    More Ways of Creating and Filling Arrays

---
+++ MoreWaysOfCreatingAndFillingArrays/script.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const account1 = {
+  owner: "Dmytro Filonenko",
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: "Maria Trofymenko",
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5, // %
+  pin: 2222,
+};
+
+const account3 = {
+  owner: "Petrenko Denis Oleksandrovich",
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7, // %
+  pin: 3333,
+};
+
+const account4 = {
+  owner: "Oleg Shevchenko",
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1, // %
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Empty Arrays + fill method
+const x = new Array(7);
+console.log(x);
+console.log(x.map(() => 5));
+
+x.fill(1, 3, 5);
+console.log(x);
+
+arr.fill(23, 4, 6);
+console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+const movementsUI = Array.from;
